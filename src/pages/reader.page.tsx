9b68ff09d66a64ae304adb4cb4ba6f5f7fcadd07
Aria Azadi Pour <aria.azadi.pour@protonmail.com>
2021-03-02 18:18:56 +0330

    fixed content

--- src/pages/reader.page.tsx
+++ src/pages/reader.page.tsx
@@ -51,6 +51,12 @@ interface ContentContainerProps {
 }
 
 const ContentContainer = styled.div<ContentContainerProps>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: auto;
   background: ${({background}) => background};
 `;
 
